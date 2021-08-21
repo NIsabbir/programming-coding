c3728a4bf0e07dba712ccc84518164adb493edb0
NIsabbir <81809868+NIsabbir@users.noreply.github.com>
2021-08-21 15:50:49 +0600

    Add files via upload

---
+++ programming/p11995.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<stack>
+#include<queue>
+using namespace std;
+int main()
+{
+    int a,b,t;
+    stack s;
+    queue q;
+    priority_queue p;
+    while(scanf("%d",&t)==1){
+
+        int i=1,j=1,k=1;
+    while(t--)
+    {
+        cin>>a>>b;
+    }
+
+
+
+    }
+    return 0;
+}
