b2cc6b9a4ec6c7b97664d22a6b62155f0d9fc437
NIsabbir <81809868+NIsabbir@users.noreply.github.com>
2021-08-21 15:51:40 +0600

    Add files via upload

---
+++ programming/tacA.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    while(scanf("%d",&x)!=EOF)
+    {
+        if(x>=0)
+            cout<<x<<endl;
+        else
+            cout<<"0"<<endl;
+    }
+}
