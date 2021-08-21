71bd61d4d23ce5b0dd64e2fce8acf4046b80a22e
NIsabbir <81809868+NIsabbir@users.noreply.github.com>
2021-08-21 15:53:17 +0600

    Add files via upload

---
+++ programming/tacC.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    while(scanf("%d",&x)!=EOF)
+    {
+        cout<<"W";
+        for(int i=1;i<=x;i++)
+            cout<<"o";
+        cout<<"w"<<endl;
+    }
+}
+
