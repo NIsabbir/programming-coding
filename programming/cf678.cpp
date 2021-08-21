59d6763ee89591aacc7aaeabdb83209d27f0539b
NIsabbir <81809868+NIsabbir@users.noreply.github.com>
2021-08-21 15:45:45 +0600

    Add files via upload

---
+++ programming/cf678.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+
+        for(int j=0;j<n;j++){
+         cout<<"3";
+        for(int i=0;i<n-1;i++)
+        {
+            cout<<" "<<"0";
+        }
+        cout<<endl;
+        }
+    }
+}
