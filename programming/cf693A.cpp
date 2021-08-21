448c5d99264dc5e22bdc51d93d42f0d318888dc9
NIsabbir <81809868+NIsabbir@users.noreply.github.com>
2021-08-21 15:48:08 +0600

    Add files via upload

---
+++ programming/cf693A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,a,m,h,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>m>>h>>n;
+        if(m%2==0&&h%2==0)cout<<"NO"<<endl;
+        else
+            cout<<"YES"<<endl;
+    }
+}
